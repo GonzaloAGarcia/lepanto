4b918f4d8d2865a7932780001ac47e7dc00441a8
Gonzalo Garcia <gonzagarcia14@gmail.com>
2023-06-05 21:06:48 +0200

    Support for images added

--- main.go
+++ main.go
@@ -4,14 +4,37 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
+	"strings"
 )
+
+type book struct {
+	Title string
+	Image string
+}
+
+func main() {
+	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("./books/images"))
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/books2/", books2)
+	mux.Handle("/books2/books/images/", http.StripPrefix("/books2/books/images/", fileServer))
+	mux.Handle("/books/", books())
+
+	//mux.HandleFunc("/url", file)
+
+	log.Println("Starting server on Port :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+
+}
 
 func home(w http.ResponseWriter, r *http.Request) {
 
 	//w.Write([]byte("Look upon my works ye mighty."))
 
-	ts, err := template.ParseFiles("./home.html")
+	ts, err := template.ParseFiles("./static/home.html")
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
@@ -29,23 +52,37 @@ func home(w http.ResponseWriter, r *http.Request) {
 func books() http.Handler {
 
 	// FromSlash makes the path platform-independent
-	path := http.Dir(filepath.FromSlash("/mnt/c/Users/Gonza/Documents/Books/"))
+	path := http.Dir(filepath.FromSlash("./books/"))
 	fs := http.StripPrefix("/books/", http.FileServer(path))
 
 	return fs
 }
 
-func main() {
+func books2(w http.ResponseWriter, r *http.Request) {
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+	files, err := os.ReadDir("./books/")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	mux.Handle("/books/", books())
+	books := make([]book, 0)
 
-	//mux.HandleFunc("/url", file)
+	for _, file := range files {
+		if !file.IsDir() {
+			book := book{
+				Title: file.Name(),
+				Image: filepath.Join("./books/images/", strings.ReplaceAll(file.Name(), ".epub", ".jpg")),
+			}
 
-	log.Println("Starting server on Port :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+			books = append(books, book)
+		}
+	}
 
+	ts := template.Must(template.ParseFiles("static/books.html"))
+	err = ts.Execute(w, books)
+	if err != nil {
+		log.Println("Error executing template:", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
